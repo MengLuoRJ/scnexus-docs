597f1b9f35841d37f200c442c7a0853be24ccb77
MengLuoRJ <mengluor@gmail.com>
2024-05-15 04:25:54 +0800

    deps: bump deps version

--- .vitepress/theme/BasicLayout.vue
+++ .vitepress/theme/BasicLayout.vue
@@ -11,11 +11,27 @@ const { Layout } = DefaultTheme;
         class="my-6 pt-6 flex flex-col gap-1 justify-center items-center text-sm border-solid border-0 border-t-1 border-t-[--vp-c-gutter]"
       >
         <div class="text-[--vp-c-text-1]">
-          {{ "Copyright © 2023 SCNexus" }}
+          {{ "Copyright © 2023-2024 SCNexus" }}
         </div>
         <div class="text-[--vp-c-text-2]">
-          {{ "Copyright © 2015-2023 AiurCovenant.Net" }}
+          {{ "Copyright © 2015-2024 AiurCovenant.Net" }}
         </div>
+        <div class="flex flex-wrap flex-row justify-center items-center gap-2 text-[--vp-c-text-2]">
+              <div>
+                <span>{{ "互联网ICP备案：" }}</span>
+                <a href="https://beian.miit.gov.cn/">
+                  {{ "蜀ICP备17022112号-4" }}
+                </a>
+              </div>
+              <div>
+                <span>{{ "川公网安备：" }}</span>
+                <a
+                  href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51010602002164"
+                >
+                  {{ "51010602002164号" }}
+                </a>
+              </div>
+            </div>
       </div>
     </template>
   </Layout>
