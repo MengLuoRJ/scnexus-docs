11091767a18e80b9aec30b4b8532eab58db771fc
MengLuoRJ <mengluor@gmail.com>
2024-06-13 22:53:25 +0800

    fix: 修正 vitepress 原生样式

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
 import Theme from "vitepress/theme";
 import BasicLayout from "./BasicLayout.vue";
-import "./style.css";
 import "virtual:uno.css";
 
 export default {
   ...Theme,
   Layout: BasicLayout,
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  },
 };
